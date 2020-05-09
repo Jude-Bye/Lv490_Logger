49fbf315b20df981dc7ee5b4f7cfa3030bb681af
Jude-Bye <ivan.bod99@gmail.com>
2020-05-09 07:23:59 +0300

    Added source files and cmake project

---
+++ CLion_CMake_Linux/Logger/cmake-build-debug/CMakeFiles/LoggerLibrary-d.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LoggerLibrary-d.dir/LogFundamentals.cpp.o"
+  "CMakeFiles/LoggerLibrary-d.dir/filelogger.cpp.o"
+  "CMakeFiles/LoggerLibrary-d.dir/filelogger_FileManager.cpp.o"
+  "CMakeFiles/LoggerLibrary-d.dir/filelogger_Messages.cpp.o"
+  "CMakeFiles/LoggerLibrary-d.dir/filelogger_StreamModes.cpp.o"
+  "CMakeFiles/LoggerLibrary-d.dir/library.cpp.o"
+  "libLoggerLibrary-d.pdb"
+  "libLoggerLibrary-d.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LoggerLibrary-d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Lv490_Logger
-Small Logger library that i did as an assignment as part of Lv490C++ group project.
+Small Logger library(.dll/.so) that i did as an assignment as part of Lv490C++ group project.
+
+Includes:
+- source files in ./src
+- projects for windows and linux dll:
+	- Visual Studio MSVC for Windows .dll:
+	- CLion CMake(gcc-8) project for Linux .so(compiled on Ubuntu 18.04LTS)
+
